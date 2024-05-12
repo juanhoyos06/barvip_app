08fa375304b0bae0df4ef332cf60733665467acc
sebastian gonzalez trujillo <sebastiangonza1505@hotmail.com>
2024-05-11 20:39:10 -0500

    feat: fix in getUserByEmail function

--- lib/controllers/AuthController.dart
+++ lib/controllers/AuthController.dart
@@ -3,6 +3,7 @@ import 'package:barvip_app/controllers/UserProvider.dart';
 import 'package:barvip_app/models/User.dart';
 import 'package:barvip_app/utils/MyStyles.dart';
 import 'package:barvip_app/views/pages/DashBoardBarberPage.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart' as auth;
 import 'package:flutter/material.dart';
 import 'package:barvip_app/models/User.dart';
@@ -10,6 +11,8 @@ import 'package:barvip_app/models/User.dart';
 class authController {
   UserController _userController = UserController();
   MyStyles myStyles = MyStyles();
+  final auth.FirebaseAuth _auth = auth.FirebaseAuth.instance;
+  final FirebaseFirestore db = FirebaseFirestore.instance;
 
   void loginGoogle(
     auth.UserCredential credential,
@@ -29,9 +32,10 @@ class authController {
       urlImage: credential.user?.photoURL ?? 'default_image_url',
     );
 
-    var existingUser = await _userController.getUserByEmail(newUser.email);
+    final Map<String, dynamic> existingUser =
+        await _userController.getUserByEmail(newUser.email);
 
-    if (existingUser == false) {
+    if (existingUser['success'] == false) {
       userProvider.userFromDb(newUser);
       await _userController.saveData(newUser.toJson());
       ScaffoldMessenger.of(context).showSnackBar(
@@ -43,8 +47,18 @@ class authController {
       ));
       return;
     } else {
+      User newUser = User(
+        name: existingUser['data']['name'],
+        lastName: existingUser['data']['lastName'],
+        email: existingUser['data']['email'],
+        password: existingUser['data']['password'],
+        typeUser: existingUser['data']['typeUser'],
+        urlImage: existingUser['data']['urlImage'],
+      );
+      var name = existingUser['data']['name'];
+      var lastName = existingUser['data']['lastName'];
       userProvider.userFromDb(newUser);
-      print("Welcome ${newUser.name} ${newUser.lastName}");
+      print("Welcome ${name} ${lastName}");
       ScaffoldMessenger.of(context).showSnackBar(
         myStyles.snackbar(
             "Welcome ${newUser.name} ${newUser.lastName}", Colors.green),
